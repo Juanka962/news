bc3f180c5b286709eacb9e4e00da7adbfa5705cb
Juan <juankaherreragarciaa@gmail.com>
2022-03-03 10:25:04 +0100

    Correcciones hasta tema 15

--- articles/urls.py
+++ articles/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import(
+from .views import (
     ArticleListView,
     ArticleUpdateView,
     ArticleDetailView,
     ArticleDeleteView,
-    ArticleCreateView,  # new
+    ArticleCreateView, # new
 )
 
 urlpatterns = [
-    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),    # new
-    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),       # new
-    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),   # new
-    path('new/', ArticleCreateView.as_view(), name='article_new'),
+    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('/new/', ArticleCreateView.as_view(), name='article_new'), # new
     path('', ArticleListView.as_view(), name='article_list'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,37 +1,37 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import (CreateView, UpdateView, DeleteView)
 from django.urls import reverse_lazy
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView): # new
     model = Article
     template_name = 'article_list.html'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView): # new
     model = Article
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView): # new
     model = Article
-    fields = ['title', 'body', ]
+    fields = ('title', 'body',)
     template_name = 'article_edit.html'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView): # new
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
-from django.views.generic.edit import (
-    CreateView,
-    UpdateView,
-    DeleteView
-)
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body', 'author')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
